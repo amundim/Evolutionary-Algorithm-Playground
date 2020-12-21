9235ea5b2b3ae35fba75731405096bd9f3092a40
Alexandre Mundim <45152248+amundim@users.noreply.github.com>
2020-12-21 11:36:13 -0300

    Add files via upload

--- selection_methods.py
+++ selection_methods.py
@@ -1,26 +1,28 @@
 import numpy as np
 import random
 
+
 def roulette_selection_method(fx_input, optimization_type_input, n_individuals_input, seed_input):
+    """Classical roulette selection method"""
 
     random.seed(seed_input)
-    
+
     roulette_sum = sum(fx_input)
     roulette_sum = np.array(roulette_sum)
     roulette_mean = np.mean(fx_input)
     roulette_max = np.max(fx_input)
-    
-    if optimization_type_input == 'max': # Se max
+
+    if optimization_type_input == 'max':
         fi_over_roulette_sum = fx_input/roulette_sum
         prob_if_max = fi_over_roulette_sum
-        roulette_cum_probability = np.cumsum(prob_if_max)  
-    else: # Se min
+        roulette_cum_probability = np.cumsum(prob_if_max)
+    else:
         prob_if_min = ((roulette_sum/fx_input)/(sum(roulette_sum/fx_input)))
-        roulette_cum_probability = np.cumsum(prob_if_min)  
+        roulette_cum_probability = np.cumsum(prob_if_min)
 
     parents_idxs = []
 
-    # Performa os trials:
+    # Perform trials:
     n_roulette_spin = 0
     while n_roulette_spin < n_individuals_input:
         idx = (np.abs(roulette_cum_probability - random.uniform(0, 1))).argmin()
@@ -31,25 +33,26 @@ def roulette_selection_method(fx_input, optimization_type_input, n_individuals_i
 
 
 def log_roulette_selection_method(fx_input, optimization_type_input, n_individuals_input, seed_input):
+    """Roulette selection method with a twist. Here the evaluation value is processed with a log function. It reduces the difference between individuals, which increases population diversity."""
 
     random.seed(seed_input)
-    
+
     roulette_sum = sum(np.log(fx_input))
     roulette_sum = np.array(roulette_sum)
     roulette_mean = np.mean(np.log(fx_input))
     roulette_max = np.max(np.log(fx_input))
-    
-    if optimization_type_input == 'max': # Se max
+
+    if optimization_type_input == 'max':
         fi_over_roulette_sum = np.log(fx_input)/roulette_sum
         prob_if_max = fi_over_roulette_sum
-        roulette_cum_probability = np.cumsum(prob_if_max)  
-    else: # Se min
+        roulette_cum_probability = np.cumsum(prob_if_max)
+    else:
         prob_if_min = ((roulette_sum/np.log(fx_input))/(sum(roulette_sum/np.log(fx_input))))
-        roulette_cum_probability = np.cumsum(prob_if_min)  
+        roulette_cum_probability = np.cumsum(prob_if_min)
 
     parents_idxs = []
 
-    # Performa os trials:
+    # Perform trials
     n_roulette_spin = 0
     while n_roulette_spin < n_individuals_input:
         idx = (np.abs(roulette_cum_probability - random.uniform(0, 1))).argmin()
@@ -58,30 +61,28 @@ def log_roulette_selection_method(fx_input, optimization_type_input, n_individua
 
     return parents_idxs
 
+
 def tournament_selection_method(fx_input, optimization_type_input, n_individuals_input, seed_input):
-    # Gera dois arrays com números aleatórios   
-    # Compara qual indivíduo tem a melhor performance conforme tipo de otimização
+    """Classical tournament selection method"""
 
     random.seed(seed_input)
-    
-    parents =  np.random.randint(len(fx_input), size=(2, len(fx_input)))
+
+    parents = np.random.randint(len(fx_input), size=(2, len(fx_input)))
     idx_a = parents[0, :]
     idx_b = parents[1, :]
     fx_a = np.array(fx_input)[idx_a.astype(int)]
     fx_b = np.array(fx_input)[idx_b.astype(int)]
 
-    # se max:
     if optimization_type_input == 'max':
         selected = np.array(fx_a) > np.array(fx_b)
-    # se min:
     else:
         selected = np.array(fx_a) < np.array(fx_b)
 
     parents_idxs = []
     for idx, item in enumerate(selected):
-        if item == True:
+        if item is True:
             parents_idxs.append(idx_a[idx])
         else:
             parents_idxs.append(idx_b[idx])
-            
+
     return parents_idxs
